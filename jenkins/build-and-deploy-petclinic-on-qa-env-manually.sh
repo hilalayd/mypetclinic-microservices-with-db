1216c56fe05d897d2eb1370eed2378978af03569
hilalayd <hilalsyn@gmail.com>
2022-06-30 15:58:23 +0000

    added jenkinsfile petclinic-weekly-qa for release branch

--- jenkins/build-and-deploy-petclinic-on-qa-env-manually.sh
+++ jenkins/build-and-deploy-petclinic-on-qa-env-manually.sh
@@ -1,6 +1,6 @@
 PATH="$PATH:/usr/local/bin"
 APP_NAME="petclinic"
-APP_REPO_NAME="clarusway-repo/petclinic-app-qa"
+APP_REPO_NAME="clarusway1-repo/petclinic-app-qa"
 ANS_KEYPAIR="matt-${APP_NAME}-qa.key"
 AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query Account --output text)
 export AWS_REGION="us-east-1"
